3d758beb3bc7a6edb51029e58c49664cb866f63b
Alex <strelchenko.aleksandr@yandex.ru>
2024-11-05 14:04:44 +0200

    feat: solve the 21st task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -360,8 +360,10 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const array = str.split(' ');
+  const result = array.map((item) => item.split('').reverse().join(''));
+  return result.join(' ');
 }
 
 /**
