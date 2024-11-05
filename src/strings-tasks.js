e2beac9c4ed025d8702e55db49fa395280722ba8
Alex <strelchenko.aleksandr@yandex.ru>
2024-11-05 10:34:25 +0200

    feat: solve the 11th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -182,8 +182,13 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(string) {
+  if (!string) return 0;
+  let result = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    result += string.charAt(i).charCodeAt();
+  }
+  return result;
 }
 
 /**
