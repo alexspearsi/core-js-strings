45b88c225a2787f590fc9bad5517fb7af9574424
Alex <strelchenko.aleksandr@yandex.ru>
2024-11-05 09:56:47 +0200

    feat: solve the 9th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,10 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const result = str.indexOf(value);
+  const innerValue = str.slice(result, value.length + result);
+  return str.replace(innerValue, '');
 }
 
 /**
